158402304ac5875613ed2d9b360a5bb26ffaa9c7
Mikhail Novikov <freiksenet@gmail.com>
2017-09-04 18:53:50 +0300

    Add search

--- github-stars-app/SearchRepositories.js
+++ github-stars-app/SearchRepositories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { gql, graphql } from 'react-apollo';
 import { View, TextInput, StyleSheet, Button } from 'react-native';
 import ListOfRepositories from './ListOfRepositories';
 import LoadingScreen from './LoadingScreen';
@@ -19,9 +20,21 @@ export default class SearchRepositories extends React.Component {
     });
   };
 
+  handlePerformSearch = () => {
+    if (this.state.searchText.length > 3) {
+      this.setState({
+        activeSearch: this.state.searchText,
+      });
+    }
+  };
+
   renderResult() {
     if (this.state.activeSearch) {
-      return <ListOfRepositories />;
+      return (
+        <SearchRepositoriesResultContainer
+          searchText={this.state.activeSearch}
+        />
+      );
     }
   }
 
@@ -34,13 +47,89 @@ export default class SearchRepositories extends React.Component {
             value={this.state.searchText}
             onChangeText={this.handleChange}
           />
-          <Button title="Search" />
+          <Button title="Search" onPress={this.handlePerformSearch} />
         </View>
         {this.renderResult()}
       </View>
     );
   }
 }
+
+class SearchRepositoriesResult extends React.Component {
+  loadMoreEntries = () => {
+    // Don't repeat it when loading
+    if (!this.props.loading) {
+      const search = this.props.data.search;
+      // if there is no more data, do nothing
+      if (!search.pageInfo.hasNextPage) {
+        return;
+      }
+      return this.props.data.fetchMore({
+        query: SearchQuery,
+        variables: {
+          // pass current last cursor
+          cursor: search.pageInfo.endCursor,
+          query: this.props.searchText,
+        },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          const newItems = fetchMoreResult.search.nodes;
+          return {
+            // return the update set
+            search: {
+              ...fetchMoreResult.search,
+              // append newly received items to the old ones
+              nodes: [...previousResult.search.nodes, ...newItems],
+            },
+          };
+        },
+      });
+    }
+  };
+
+  render() {
+    if (this.props.data.loading) {
+      return <LoadingScreen />;
+    } else {
+      return (
+        <ListOfRepositories
+          repositories={this.props.data.search}
+          refreshing={this.props.data.loading}
+          onRefresh={() => this.props.data.refetch()}
+          onLoadMore={this.loadMoreEntries}
+        />
+      );
+    }
+  }
+}
+
+const SearchQuery = gql`
+  query SearchQuery($cursor: String, $query: String!) {
+    search(type: REPOSITORY, query: $query, first: 10, after: $cursor) {
+      repositoryCount
+      nodes {
+        ... on Repository {
+          id
+          nameWithOwner
+          viewerHasStarred
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+`;
+
+const SearchRepositoriesResultContainer = graphql(SearchQuery, {
+  options: props => {
+    return {
+      variables: {
+        query: props.searchText,
+      },
+    };
+  },
+})(SearchRepositoriesResult);
 
 const styles = StyleSheet.create({
   inputContainer: {
@@ -50,7 +139,9 @@ const styles = StyleSheet.create({
     paddingRight: 10,
   },
   input: {
+    backgroundColor: 'white',
     fontSize: 15,
-    height: 20,
+    height: 40,
+    paddingHorizontal: 5,
   },
 });
